15a132b1334e912a461dea4299d9b72417d9f273
Ale-ssandra <lele.p.cunha@gmail.com>
2023-12-06 17:08:23 -0300

    componente criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import {PrimeiroComponenteComponent} from "./components/primeiro-componente/primeiro-componente.component"
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet , PrimeiroComponenteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
