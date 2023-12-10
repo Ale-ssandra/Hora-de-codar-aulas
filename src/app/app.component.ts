2b7e8b56cd1250b313f86dedcdaa649dc6484d1f
Ale-ssandra <lele.p.cunha@gmail.com>
2023-12-10 12:06:02 -0300

    commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {PrimeiroComponenteComponent} from "./components/primeiro-componente/primeiro-componente.component";
-import {DadosNoTemplateComponent} from "./components/dados-no-template/dados-no-template.component"
+import {DadosNoTemplateComponent} from "./components/dados-no-template/dados-no-template.component";
+import{CompartilhamentoDeDadosComponent} from "./components/compartilhamento-de-dados/compartilhamento-de-dados.component"
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet , PrimeiroComponenteComponent , DadosNoTemplateComponent],
+  imports: [CommonModule, RouterOutlet , PrimeiroComponenteComponent , DadosNoTemplateComponent ,CompartilhamentoDeDadosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AulasHoraDeCodar';
+  @Input() name: string = ""
 }
---
+++ src/app/components/compartilhamento-de-dados/compartilhamento-de-dados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compartilhamento-de-dados',
+  standalone: true,
+  imports: [],
+  templateUrl: './compartilhamento-de-dados.component.html',
+  styleUrl: './compartilhamento-de-dados.component.css'
+})
+export class CompartilhamentoDeDadosComponent {
+
+}
